a96492177d89b13eeb6270258986924fa1165cef
avrahamcool <essoudry@gmail.com>
2018-02-23 16:51:09 +0200

    added WebApiThrottle

--- Aleph1.Skeletons.WebAPI.WebAPI/App_Start/WebApiConfig.cs
+++ Aleph1.Skeletons.WebAPI.WebAPI/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Aleph1.Logging;
 using System.Web.Http;
+using WebApiThrottle;
 
 namespace Aleph1.Skeletons.WebAPI.WebAPI
 {
@@ -11,16 +12,22 @@ namespace Aleph1.Skeletons.WebAPI.WebAPI
         [Logged(LogParameters = false)]
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            //Apply Throttling Policy on all Controllers
+            //see more configs here: https://github.com/stefanprodan/WebApiThrottle
+            config.MessageHandlers.Add(new ThrottlingHandler()
+            {
+                Policy = new ThrottlePolicy(perSecond: 1, perMinute: 45, perHour: 2_025, perDay: 18_225, perWeek: 95_682)
+                {
+                    IpThrottling = true,
+                    EndpointThrottling = true,
+                    StackBlockedRequests = true
+                },
+                Repository = new CacheRepository()
+            });
         }
     }
 }
